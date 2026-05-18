ce9454ff0bdfa2457dac9c0a53d8ae378dddbd0c
Yunosuke Sakai <qmkns246@gmail.com>
2026-05-18 20:53:48 +0900

    Initial Commit

---
+++ NASAClientObservationVer/NASAClientObservationVerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAClientObservationVerApp.swift
+//  NASAClientObservationVer
+//
+//  Created by Yunosuke Sakai on 2026/05/18.
+//
+
+import SwiftUI
+
+@main
+struct NASAClientObservationVerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NASAClientObservationVerTests/NASAClientObservationVerTests.swift
@@ -0,0 +1,19 @@
+//
+//  NASAClientObservationVerTests.swift
+//  NASAClientObservationVerTests
+//
+//  Created by Yunosuke Sakai on 2026/05/18.
+//
+
+import Testing
+@testable import NASAClientObservationVer
+
+struct NASAClientObservationVerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
