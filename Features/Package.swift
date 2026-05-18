5845f2a384a44f1c40b4be2a8470179e649789ac
Yunosuke Sakai <qmkns246@gmail.com>
2026-05-18 22:36:29 +0900

    Add `AstronomyPictureDetail` module

--- Features/Package.swift
+++ Features/Package.swift
@@ -8,6 +8,10 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
+            name: "AstronomyPictureDetail",
+            targets: ["AstronomyPictureDetail"],
+        ),
+        .library(
             name: "Features",
             targets: ["Features"]
         )
@@ -15,6 +19,14 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AstronomyPictureDetail",
+            dependencies: [],
+        ),
+        .testTarget(
+            name: "AstronomyPictureDetailTests",
+            dependencies: ["AstronomyPictureDetail"],
+        ),
         .target(
             name: "Features"
         ),
