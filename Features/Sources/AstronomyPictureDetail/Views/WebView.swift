9d2cbb62ae265e798db28e6ba7952110adbd0a10
Yunosuke Sakai <qmkns246@gmail.com>
2026-05-18 22:53:42 +0900

    Add `WebView`

---
+++ Features/Sources/AstronomyPictureDetail/Views/WebView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context _: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+    
+    func updateUIView(_: WKWebView, context _: Context) {}
+}
+
+#Preview {
+    WebView(url: .init(string: "https://www.youtube.com/embed/V_Kd4YBNs7c?rel=0")!)
+}
